cb6a628fbde8f4bd05d488981942b8ea02d63e36
timmythetiny <account@timharding.co>
2019-07-26 13:34:13 -0700

    Full development environment.

---
+++ backend/app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api", apiRoot)
+	http.ListenAndServe(":8080", router)
+}
+
+func apiRoot(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "This is the RESTful api")
+}
